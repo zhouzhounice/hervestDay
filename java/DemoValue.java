fee1862642fcbe103b75ad0c3642fbf00e539436
anan <894425013@qq.com>
2022-10-18 22:49:58 +0800

    java学习

---
+++ java/DemoValue.java
@@ -0,0 +1,10 @@
+public class DemoValue{
+	public static void main(String[] args){
+		System.out.println(666);
+		System.out.println(-6);
+		System.out.println(6.66);
+		System.out.println(-66.6);
+		System.out.println("djskalf");
+		System.out.println('a');
+	}
+}
