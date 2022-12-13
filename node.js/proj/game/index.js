551154e406e801f9ce7cc3ca7875c17bd275e3de
anan <894425013@qq.com>
2022-12-13 22:37:42 +0800

    小游戏

--- node.js/proj/game/index.js
+++ node.js/proj/game/index.js
@@ -12,11 +12,26 @@ let playerLastAction = null;
 let sameCount = 0;
 
 http.createServer(function(request, response){
-  const parsedUrl = new URL(request.url,"http://whatever.com");
+  const parsedUrl = url.parse(request.url);
 
   if(parsedUrl.pathname == '/favicon.ico'){
     response.writeHead(200);
     response.end();
     return;
   }
-})
+  if(parsedUrl.pathname === '/game'){
+    const query = querystring.parse(parsedUrl.query);
+    const playerAction = query.action;
+    const gameResult = game(playerAction);
+    console.log(game(playerAction));
+
+    response.writeHead(200);
+    if(gameResult == 0){
+      
+    }
+    response.end()
+  }
+  if(parsedUrl.pathname == '/'){
+    fs.createReadStream(__dirname+'/index.html').pipe(response);
+  }
+}).listen(3000)
