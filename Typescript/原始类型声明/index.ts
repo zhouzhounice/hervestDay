bcd1a01e27e2442363eddb4a390a03da779f3cae
anan <894425013@qq.com>
2022-12-24 16:51:30 +0800

    数组类型标注-元组

--- Typescript/原始类型声明/index.ts
+++ Typescript/原始类型声明/index.ts
@@ -24,3 +24,18 @@ function b_01():void {
 function c_01():undefined {
   return undefined
 } // 返回值类型为undefined
+
+const arr1_01:string[] = ['a','b'];
+const arr2_01:Array<string> = ['a','b'];
+const arr3_01:string[] = ['a','b','c'];
+console.log(arr3_01[599]) // undefined
+
+const arr4_01:[string,number,boolean] = ['a',1,true]
+// console.log(arr4_01[5])
+
+const arr5_01: [string, number?, boolean?] = ['a', 1, true]
+type TupleLength = typeof arr5_01.length // 3 | 2 | 1
+
+// 具名元组
+const arr6_01:[name2_01:string,age:number,male:boolean] = ['anne',18,true]
+// const [name2_01, age, male, other] = arr6_01
