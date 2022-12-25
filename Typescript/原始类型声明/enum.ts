a93dc824e668d86882901ae204d2ce36bcb8252b
anan <894425013@qq.com>
2022-12-25 11:45:16 +0800

    字面量类型与联合类型的结合、枚举的使用

--- Typescript/原始类型声明/enum.ts
+++ Typescript/原始类型声明/enum.ts
@@ -6,4 +6,35 @@ enum PageUrl {
 }
 
 const home = PageUrl.Home_Page_Url;
-console.log(home)
+// console.log(home)
+
+enum Items1_05 {
+  Foo, // 0
+  Bar = 599,
+  Baz, // 600
+}
+// console.log(Items1_05.Foo)
+// console.log(Items1_05.Baz)
+
+// 数字型枚举 延迟求值
+const returnNum = () => 200 + 499;
+
+enum Items2_05 {
+  Foo = returnNum(), // 699
+  // test, //会报错 
+  Bar = 599, //599
+  Baz // 600
+}
+
+// console.log(Items2_05.Foo)
+// console.log(Items2_05.Bar)
+// console.log(Items2_05.Baz)
+
+enum Mixed1_05 {
+  Num1 = 300,
+  Num2 = 300,
+  Str = 'anne'
+}
+
+console.log(Mixed1_05.Num1)
+console.log(Mixed1_05[300])
