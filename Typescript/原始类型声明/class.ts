d914986045eddf8677dc596a88e0062622f338ad
anan <894425013@qq.com>
2022-12-26 15:47:40 +0800

    类的新语法

--- Typescript/原始类型声明/class.ts
+++ Typescript/原始类型声明/class.ts
@@ -57,3 +57,78 @@ class Foo4_07 {
 }
 // 此时参数会被直接作为类的成员(即实例的属性值)，免去后续的手动赋值
 new Foo4_07('anne',true)
+
+//静态成员
+class Foo5_07 {
+  static staticHandle(){
+    console.log('访问静态成员')
+  }
+  public instanceHandle(){
+    console.log(1)
+  }
+}
+
+// 在类的内部静态成员无法通过this来访问，需要通过
+Foo5_07.staticHandle()
+
+// 编译之后的类的代码
+/* 
+var Foo5_07 = (
+  function (){
+    function Foo5_07(){};
+    Foo5_07.staticHandler = function(){};
+    Foo5_07.prototype.instanceHandle = function (){};
+    return Foo5_07
+  }
+)()
+
+*/
+
+// 派生类覆盖基类方法时
+class Foo6_07 extends Foo5_07 {
+  instanceHandle(){
+    console.log(2);
+    super.instanceHandle()
+  }
+}
+
+Foo6_07.prototype.instanceHandle()
+
+// override
+class Base {
+  printWithLove(){}
+}
+
+class Derived extends Base {
+  // override print(){}
+  override printWithLove() {}
+}
+
+// 抽象类型使用abstract关键字声明
+abstract class Foo7_07 {
+  abstract absProp:string;
+  abstract get absGetter():string;
+  abstract absMethod(name:string):string;
+}
+
+class Foo8_07 implements Foo7_07 {
+  absProp: string = 'anne'
+
+  get absGetter() {
+    return 'amm'
+  }
+
+  absMethod(name: string): string {
+    return name
+  }
+}
+
+// Newable Interface
+class Foo {}
+interface FooStruct {
+  new():Foo
+}
+
+declare const NewableFoo:FooStruct;
+
+const foo = new NewableFoo()
