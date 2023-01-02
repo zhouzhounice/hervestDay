97d3f76102f7c196bb2ffcac62e6a0dcece91a17
anan <894425013@qq.com>
2023-01-02 22:28:47 +0800

    简单的todo案例

--- Vue3/vite-project/src/App.vue
+++ Vue3/vite-project/src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import TodoList from './components/Todolist.vue';
 </script>
 
--- Vue3/vite-project/src/components/Todolist.vue
+++ Vue3/vite-project/src/components/Todolist.vue
@@ -1,8 +1,7 @@
-
 <template>
   <div>
     <input type="text" v-model="title" @keydown.enter="addTodo" />
-    <button v-if="active < all" @click="clear">清理</button>
+    <button  @click="clear">清理</button>
     <ul v-if="todos.length">
       <li v-for="todo in todos" :key="todo.key">
         <input type="checkbox" v-model="todo.done" />
@@ -11,8 +10,8 @@
     </ul>
     <div v-else>暂无数据</div>
     <div>
-      全选<input type="checkbox" v-model="allDone" />
-      <span> {{ active }} / {{ all }} </span>
+      全选<input type="checkbox" v-model="allDone">
+      <span>{{ active }} / {{ all }}</span>
     </div>
   </div>
 </template>
@@ -20,26 +19,34 @@
 <script setup>
 import { ref,computed } from "vue";
 let title = ref("");
-let todos = ref([{title:'学习Vue',done:false,key:"0"}])
+let todos = ref([{title:'学习Vue',done:true,key:'0'}])
 
 function addTodo() {
-  todos.value.push({    title: title.value,    done: false, key:`${Date.parse(new Date())}`  });  title.value = "";
+  todos.value.push({
+    title: title.value,
+    done: false,
+    key:`${new Date().getTime()}`
+  });
+  title.value = "";
 }
-function clear() {
-  todos.value = todos.value.filter((v) => !v.done);
+function clear(){
+  todos.value = todos.value.filter((v)=>!v.done)
 }
-let active = computed(() => {
-  return todos.value.filter((v) => !v.done).length;
+let active = computed(()=> {
+  return todos.value.filter((v)=> v.done).length
 });
-let all = computed(() => todos.value.length);
+
+let all = computed(()=> todos.value.length)
+
 let allDone = computed({
-  get: function () {
-    return active.value === 0;
+  get:function(){
+    return active.value !== 0;
   },
-  set: function (value) {
-    todos.value.forEach((todo) => {
-      todo.done = value;
-    });
-  },
-});
+  set:function(value){
+    console.log(value)
+    todos.value.forEach((todo)=>{
+      todo.done = value
+    })
+  }
+})
 </script>
