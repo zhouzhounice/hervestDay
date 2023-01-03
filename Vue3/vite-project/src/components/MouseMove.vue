05636a91711c7a20c4ddeb9d8321a2476a9b31c5
zhouzhounice <894425013@qq.com>
2023-01-03 12:35:07 +0800

    颜色使用v-bind切换dome

--- Vue3/vite-project/src/components/MouseMove.vue
+++ Vue3/vite-project/src/components/MouseMove.vue
@@ -1,5 +1,14 @@
 <script setup>
+import {ref} from 'vue';
 import {useMouse} from '../utils/useMouse';
+let count = ref(1);
+let color = ref('pink');
+
+function add(){
+  console.log(count)
+  count.value++;
+  color.value = Math.random()>0.5?'yellowgreen':'pink'
+}
 let {x,y} = useMouse()
 </script>
 
@@ -7,9 +16,15 @@ let {x,y} = useMouse()
     <div>
       <h1>当前横坐标的值为{{ x }}</h1>
       <h1>当前纵坐标的值为{{ y }}</h1>
+      <h2 @click="add">点击此处切换颜色{{ count }}</h2>
     </div>
 </template>
 
-<style lang='less' scoped>
-
+<style scoped>
+  h1{
+    color:plum;
+  }
+  h2{
+    color:v-bind(color)
+  }
 </style>
