08051f573487341b130e637ead92c1a12229fc08
Anne <894425013@qq.com>
2023-01-10 14:14:00 +0800

    封装一个方法

--- Vue3/vite-project/src/pages/home.vue
+++ Vue3/vite-project/src/pages/home.vue
@@ -3,18 +3,52 @@
     <h1 class="hover" @click="toHome">Home</h1>
     <h1>|</h1>
     <h1 class="hover" @click="toAbout">About</h1>
+
+    <button @click="handle">点我</button>
   </div>
 </template>
 
 
 <script setup>
 import { useRouter } from "vue-router";
+import toArry from '../utils/test';
 const router = useRouter()
+
+const obj = {
+  'obj1':[
+    {
+      statName:"smoke",
+      statTime:"2023-01-84",
+      statValue:0,
+    },
+    {
+      statName:"smoke",
+      statTime:"2023-01-84",
+      statValue:20,
+    },
+  ],
+  'obj2':[
+    {
+      statName:"smoke",
+      statTime:"2023-01-84",
+      statValue:30,
+    },
+    {
+      statName:"smoke",
+      statTime:"2023-01-84",
+      statValue:40,
+    },
+  ],
+
+}
 const toHome = () =>{
   router.push('/')
 }
 const toAbout = () =>{
   router.push('/about')
+}
+const handle = () =>{
+  toArry(obj)
 }
 </script>
 
---
+++ Vue3/vite-project/src/utils/test.js
@@ -0,0 +1,63 @@
+const obj = {
+  'obj1':[
+    {
+      statName:"smoke",
+      statTime:"2023-01-84",
+      statValue:0,
+    },
+    {
+      statName:"smoke",
+      statTime:"2023-01-84",
+      statValue:20,
+    },
+  ],
+  'obj2':[
+    {
+      statName:"smoke",
+      statTime:"2023-01-84",
+      statValue:30,
+    },
+    {
+      statName:"smoke",
+      statTime:"2023-01-84",
+      statValue:40,
+    },
+  ],
+
+}
+
+const arr = [
+  {
+    name:'obj1',
+    label:{
+      show:true,
+      fontSize:8,
+      distance:0,
+      position:'top'
+    },
+    data:[0,20],
+    type:'bar'
+  },
+  {
+    name:'obj2',
+    label:{},
+    data:[30,40],
+    type:'bar'
+  }
+]
+
+export default function toArry(obj){
+  const isObj = Object.prototype.toString.call(obj) === '[object Object]'
+  if(!isObj) return
+  return Object.keys(obj).map(item =>  ({
+    name:item,
+    label:{
+      show:true,
+      fontSize:8,
+      distance:0,
+      position:'top'
+    },
+    data:obj[item].map(it => it.statValue),
+    type:'bar'
+  }))
+}
