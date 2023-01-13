7115bdecc490910c7c35637ee778c0c2682538b1
zhouzhounice <894425013@qq.com>
2023-01-13 11:57:04 +0800

    修改头部导航栏样式

--- Vue3/vite-project/src/App.vue
+++ Vue3/vite-project/src/App.vue
@@ -1,3 +1,4 @@
+import { Mode } from 'use-switch-tabs';
 <template>
   <!-- <el-container>
     <el-header>Header</el-header>
@@ -18,7 +19,43 @@
     </el-container>
   </el-container> -->
   <el-container>
-  <el-header>Header</el-header>
+  <el-header>
+    <el-menu
+      :default-active = '1'
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="1">
+        处理中心
+      </el-menu-item>
+      <el-submenu index="2">
+        <template v-slot:title>
+          我的工作台
+        </template>
+        <el-menu-item index="2-1">
+          菜单1
+        </el-menu-item>
+        <el-menu-item index="2-2">
+          菜单2
+        </el-menu-item>
+        <el-menu-item index="2-3">
+          菜单3
+        </el-menu-item>
+        <el-submenu index="2-4">
+          <template v-solt:title>
+            菜单4
+          </template>
+          <el-menu-item index="2-4-1">子菜单1</el-menu-item>
+          <el-menu-item index="2-4-2">子菜单2</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3">消息中心</el-menu-item>
+      <el-menu-item index="4">订单管理</el-menu-item>
+    </el-menu>
+  </el-header>
   <el-container>
     <el-aside width="200px">
       <div>
