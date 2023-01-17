596d9e6b613ff64b2f55b5616d932fa8d2150288
zhouzhounice <894425013@qq.com>
2023-01-17 13:46:20 +0800

    了解闭包

--- 浏览器工作原理与实践/function.js
+++ 浏览器工作原理与实践/function.js
@@ -11,25 +11,41 @@
 
 // foo()
 // closure
-function foo(){
-  var myName = 'A';
-  let test = 2;
-  var innerBar = {
-    getName:function(){
-      console.log(test)
-      return myName
-    },
-    setName:function(newName){
-      myName = newName
-    }
-  }
-  return innerBar
-}
+// function foo(){
+//   var myName = 'A';
+//   let test = 2;
+//   var innerBar = {
+//     getName:function(){
+//       console.log(test)
+//       return myName
+//     },
+//     setName:function(newName){
+//       myName = newName
+//     }
+//   }
+//   return innerBar
+// }
 
-var bar = foo()
-bar.setName('极客邦');
-bar.getName()
-console.log(bar.getName())
+// var bar = foo()
+// bar.setName('极客邦');
+// bar.getName()
+// console.log(bar.getName())
+
+
+var bar = {
+    myName:"time.geekbang.com",
+    printName: function () {
+        console.log(myName)
+    }    
+}
+function foo() {
+    let myName = "极客时间"
+    return bar.printName
+}
+let myName = "极客邦"
+let _printName = foo()
+_printName()
+bar.printName()
 
 
 
