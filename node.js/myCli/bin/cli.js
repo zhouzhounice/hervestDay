9c3bfca588619afb1e576dcb5ff1c2128161be38
zhouzhounice <894425013@qq.com>
2023-01-24 22:50:42 +0800

    处理自定义命令参数 实现创建项目

--- node.js/myCli/bin/cli.js
+++ node.js/myCli/bin/cli.js
@@ -13,4 +13,14 @@ const { program } = require('commander');
 
 // 设置一个指令参数
 program.option('-f --framwork <framwork>','设置框架')
+// 设置一个创建脚手架的命令
+program
+.command('create <project> [other...]')
+.alias('crt')
+.description('创建项目')
+.action((project,args)=>{
+  // 命令行的执行逻辑代码
+  console.log(project)
+  console.log(args)
+})
 program.parse(process.argv)
