ba1293dd2d40651fbb7b426d5e49e62c27d81cc2
zhouzhounice <894425013@qq.com>
2023-01-24 22:27:52 +0800

    简单使用commander工具管理命令参数

--- node.js/myCli/bin/cli.js
+++ node.js/myCli/bin/cli.js
@@ -1,5 +1,16 @@
 #! /usr/bin/env node
 
-console.log('myCli')
+// console.log('myCli')
 
-//
+// 与命令行做交互
+// console.log(process.argv[2])
+// const str = process.argv[2]
+// if(str==='--help'){
+//   console.log('获取到命令参数')
+// }
+
+const { program } = require('commander');
+
+// 设置一个指令参数
+program.option('-f --framwork <framwork>','设置框架')
+program.parse(process.argv)
