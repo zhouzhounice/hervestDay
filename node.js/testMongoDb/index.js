6d003dbfbda8741fc0f15b77097f5d1130a728e5
zhouzhounice <894425013@qq.com>
2023-02-07 13:24:35 +0800

    使用node与redis做一个链接

--- node.js/testMongoDb/index.js
+++ node.js/testMongoDb/index.js
@@ -1,10 +1,19 @@
-const { MongoClient } = require('mongodb');
-const client = new MongoClient('mongodb://127.0.0.1:27017');
-const main = async ()=>{
-await client.connect()
-const db =  client.db('mytest');
-const cc = db.collection('cc');
-var d = await cc.find()
-console.log(await d.toArray());
-}
-main().finally(()=>client.close()) // 断开连接
+// const { MongoClient } = require('mongodb');
+// const client = new MongoClient('mongodb://127.0.0.1:27017');
+// const main = async ()=>{
+// await client.connect()
+// const db =  client.db('mytest');
+// const cc = db.collection('cc');
+// var d = await cc.find()
+// console.log(await d.toArray());
+// }
+// main().finally(()=>client.close()) // 断开连接
+
+const Redis = require('ioredis');
+
+const redis = new Redis();
+redis.set("mykey","value");
+
+console.log(redis.keys('*').then(res =>{
+  console.log(res)
+}))
