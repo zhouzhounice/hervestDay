c4552a84a2e96da1e7699e2afa9cd2c8709b9b81
zhouzhounice <894425013@qq.com>
2023-02-18 17:25:41 +0800

    初始化响应式文件

---
+++ Vue3/ailemente/src/reactive/effect.js
@@ -0,0 +1,9 @@
+
+export function effect(target) {
+  if (typeof target!=='object') {
+    console.warn(`reactive  ${target} 必须是一个对象`);
+    return target
+  }
+
+  return new Proxy(target, mutableHandlers);
+}
