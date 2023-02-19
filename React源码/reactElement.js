4865e4a7fa929376a820093b03c863f006cbef72
zhouzhounice <894425013@qq.com>
2023-02-19 23:57:55 +0800

    react源码解读1

---
+++ React源码/reactElement.js
@@ -0,0 +1,25 @@
+export function createElement(type,config,children){
+	 // 处理参数
+	return ReactElement(
+		type,
+		key,
+		ref,
+		self,
+		source,
+		ReactCurrentOwner.current,
+		props
+	)
+}
+
+const ReactElement = function (type,key,ref,self,source,owner,props){
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		
+		type,
+		key,
+		ref,
+		props,
+		_owner:owner
+	}
+	return element
+}
