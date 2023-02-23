35238a9d0ece9f855a391fbaad8acad2c5df120a
zhouzhounice <894425013@qq.com>
2023-02-23 22:54:11 +0800

    实现脚手架的初步分包

--- cli-test/first-cli/bin/index.js
+++ cli-test/first-cli/bin/index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
-
-console.log('welcom my-cli');
+const lib = require("fivst-cli-lib");
+console.log(lib);
+// 注册一个命名 imooc-test init
+const argv = require('process').argv;
+const command = argv[2]
+console.log(command)
+if(command){
+  if(lib[command]){
+    lib[command]();
+  }else{
+    console.log('无效的命令')
+  }
+}else{
+  console.log('请输入命令')
+}
+// 实现参数解析 --version 和 init --name
