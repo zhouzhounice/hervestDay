710d5c86565072a70fdb07a2525dd34b3f62dd03
zhouzhounice <894425013@qq.com>
2023-02-27 00:48:16 +0800

    node版本检查

--- cli-test/second-cli/packages/cli/lib/index.js
+++ cli-test/second-cli/packages/cli/lib/index.js
@@ -1,10 +1,25 @@
 const commander = require("commander");
-const createInitCommand = require('@anne.cn/init')
+const createInitCommand = require('@anne.cn/init');
+const semver = require('semver')
 
 const { program } = commander;
 const { log } = require('@anne.cn/utils')
 const pkg = require("../package.json")
+const LOWEST_NODE_VERSION = "14.0.0";
 
+function checkNodeVersion(){
+	log.verbose('node version',process.version);
+	if(!semver.gte(process.version,LOWEST_NODE_VERSION)){
+		throw new Error(`anne-cli需要安装${LOWEST_NODE_VERSION}以上的版本`)
+	}
+}
+
+function preActions(){
+	checkNodeVersion()
+}
+process.on('uncaughtException',(e)=>{
+	console.log(e.message)
+})
 module.exports = function (args){
 	log.info('version',pkg.version)
 	program
@@ -12,6 +27,7 @@ module.exports = function (args){
 		.usage("<command> [options]")
 		.version(pkg.version)
 		.option('-d, --debug','是否开启调试模式',false)
+		.hook('preAction',preActions)
 	createInitCommand(program)
 	program.parse(process.argv)
 }
--- cli-test/second-cli/packages/utils/lib/log.js
+++ cli-test/second-cli/packages/utils/lib/log.js
@@ -1,3 +1,10 @@
 const log = require('npmlog');
-
+const isDebug = require('./isDebug')
+if(isDebug){
+	log.level = 'verbose'
+}else {
+	log.level = 'info'
+}
+// log.heading = "anne";
+log.addLevel('success',2000,{ fg:'green',bold:true})
 module.exports = log;
