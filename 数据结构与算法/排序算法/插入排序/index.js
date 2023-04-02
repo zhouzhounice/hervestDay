b746841490053361ec4fb64e051e15b4421b4160
anan <894425013@qq.com>
2023-04-02 12:06:31 +0800

    数据结构与算法 直接插入算法补充

--- 数据结构与算法/排序算法/插入排序/index.js
+++ 数据结构与算法/排序算法/插入排序/index.js
@@ -1,4 +1,6 @@
 // 直接插入排序
+// 时间 复杂度最好与最坏都为O(n^2)
+
 const insertionSort = arr =>{
     const len = arr.length;
     if(len<=1) return
@@ -22,10 +24,13 @@ const insertionSort = arr =>{
 // 测试
 const array = [5, 4, 3, 2, 1];
 console.log("原始 array :", array);
-debugger
+
 insertionSort(array);
 // 原始 array:  [5, 4, 3, 2, 1]
 // array:  	 [4, 5, 3, 2, 1]
 // array: 	 [3, 4, 5, 2, 1]
 // array: 	 [2, 3, 4, 5, 1]
 // array:    [1, 2, 3, 4, 5]
+
+
+//
