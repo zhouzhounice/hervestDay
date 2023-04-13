a89fdec715b857e7209ea4e99082c174a4eca164
anan <894425013@qq.com>
2023-04-13 18:34:58 +0800

    c++基础语法之二维数组应用案例 求成绩总和

--- C++/02.c++基础语法/07.二维数组.cpp
+++ C++/02.c++基础语法/07.二维数组.cpp
@@ -3,16 +3,33 @@ using namespace std;
 
 int main() {
 	// 定义二维数组
-	int arr[2][3] =
-	{
-		{1,2,3},
-		{4,5,6}
-	};
+	//int arr[2][3] =
+	//{
+	//	{1,2,3},
+	//	{4,5,6}
+	//};
 	// 打印二维数组
-	for (int i = 0; i < 2; i++) {
+	//for (int i = 0; i < 2; i++) {
+	//	for (int j = 0; j < 3; j++) {
+	//		cout << arr[i][j] << endl;
+	//	}
+	//}
+
+	// 计算张三、李四、王五的总成绩
+	int scores[3][3] = {
+		{100,100,100},
+		{90,50,100},
+		{60,70,80}
+	};
+	string names[3] = { "张三","李四","王五" };
+
+	for (int i = 0; i < 3; i++) {
+		int sum = 0;
 		for (int j = 0; j < 3; j++) {
-			cout << arr[i][j] << endl;
+			sum += scores[i][j];
 		}
+		cout << names[i]<<"的成绩为：" << sum << " ";
+		cout << endl;
 	}
 	system("pause");
 	return 0;
