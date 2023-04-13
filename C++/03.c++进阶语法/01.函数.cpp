0ea387b2d5b12c7a1639e43f54ce988ae4cae36c
anan <894425013@qq.com>
2023-04-13 19:38:45 +0800

    c++基础语法之函数相关知识

---
+++ C++/03.c++进阶语法/01.函数.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include "swap.h"
+using namespace std;
+
+// »ù´¡º¯ÊıÓï·¨
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+	return sum;
+}
+
+int main() {
+	int a = 10;
+	int b = 20;
+	int res = add(a, b);
+	 swap(a, b);
+	cout << res << endl;
+	system("pause");
+	return 0;
+}
