af9371b9ba00160ef0bea746a4ef25792997c748
anan <894425013@qq.com>
2023-11-04 15:58:15 +0800

    根据是否选中组件动态切换属性和页面设置

--- react实现低代码平台/react-ts-demo/src/page/questions/Edit/RightPanel.tsx
+++ react实现低代码平台/react-ts-demo/src/page/questions/Edit/RightPanel.tsx
@@ -1,12 +1,26 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Tabs } from "antd";
 import { FileTextFilled, SettingOutlined } from "@ant-design/icons";
 import ComponentProp from "./components/ComponentProp";
+import PageSetting from "./components/PageSetting";
+import useGetComponentInfo from "../../../hooks/useGetComponentInfo";
+
+enum TAB_KEYS {
+  PROP_KEY = "prop",
+  SETTING_KEY = "setting",
+}
 
 const RightPanel: FC = () => {
+  const [activeKey, setActiveKey] = useState(TAB_KEYS.PROP_KEY);
+  const { selectedId } = useGetComponentInfo();
+  useEffect(() => {
+    if (selectedId) setActiveKey(TAB_KEYS.PROP_KEY);
+    else setActiveKey(TAB_KEYS.SETTING_KEY);
+  }, [selectedId]);
+
   const tabsItems = [
     {
-      key: "prop",
+      key: TAB_KEYS.PROP_KEY,
       label: (
         <span>
           <FileTextFilled />
@@ -16,17 +30,17 @@ const RightPanel: FC = () => {
       children: <ComponentProp />,
     },
     {
-      key: "setting",
+      key: TAB_KEYS.SETTING_KEY,
       label: (
         <span>
           <SettingOutlined />
           设置
         </span>
       ),
-      children: <div>页面设置</div>,
+      children: <PageSetting />,
     },
   ];
-  return <Tabs items={tabsItems} defaultActiveKey="prop"></Tabs>;
+  return <Tabs items={tabsItems} activeKey={activeKey}></Tabs>;
 };
 
 export default RightPanel;
---
+++ react实现低代码平台/react-ts-demo/src/page/questions/Edit/components/PageSetting.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const PageSetting: FC = () => {
+  return <div>页面设置</div>;
+};
+
+export default PageSetting;
