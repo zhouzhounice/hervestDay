eb461c8e279677ebcd97ace9f7f76048a4c46bbd
anan <894425013@qq.com>
2023-11-05 19:39:59 +0800

    完成统计页面头部组件

--- react实现低代码平台/react-ts-demo/src/page/questions/Stat/index.tsx
+++ react实现低代码平台/react-ts-demo/src/page/questions/Stat/index.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import useLoadQuestionData from "../../../hooks/useLoadQuestionData";
 import useGetPageInfo from "../../../hooks/useGetPageInfo";
 import styles from "./index.module.scss";
+import StatHeader from "./StatHeader";
 
 const Stat: FC = () => {
   const { loading } = useLoadQuestionData();
@@ -36,7 +37,7 @@ const Stat: FC = () => {
   };
   return (
     <div className={styles.container}>
-      <div>Header</div>
+      <StatHeader />
       <div className={styles["content-wrapper"]}>
         {loading && <Spin />}
         {!loading && <div className={styles.content}>{genContentElem()}</div>}
